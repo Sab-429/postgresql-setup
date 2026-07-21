9648ce1977246545965a6cc184ebe9c89aacd8bc
saysa <sabyasachi7629@gmail.com>
2026-07-21 19:42:03 +0530

    added signin endpoint

---
+++ Auth_middleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+
+function auth(req,res,next) {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader) {
+        return res.status(401).json({
+            message: "Token is missing"
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            message: "Invalid token"
+        })
+    }
+}
+
+module.exports = auth;
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require ('express')
 const pool = require("./db")
+const jwt = require('jsonwebtoken')
+const bcrypt = require("bcrypt")
 const app = express() 
 app.use(express.json())
 
 app.post('/signup', async(req,res) => {
    try {
         const { username, email, password}  = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
         const exitingUser = await pool.query(
             "SELECT * FROM users WHERE email = $1", [email]
         );
@@ -20,7 +23,7 @@ app.post('/signup', async(req,res) => {
             [
                 username,
                 email,
-                password
+                hashedPassword
             ]
         );
         res.status(201).json({
@@ -32,8 +35,39 @@ app.post('/signup', async(req,res) => {
         })
    }
 })
-app.post('/signin', (req,res) => {
+app.post('/signin', async(req,res) => {
+    try {
+        const { email, password } = req.body;
+        const existingUser = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email]
+        );
+        if(existingUser.rows.length === 0){
+            return res.status(404).json({
+                message: "User not found"
+            })
+        }
+        const user = existingUser.rows[0]
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch) {
+            return res.status(401).json({
+                message: "Invalid credentials"
+            })
+        }
+        const token = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, process.env.JWT_SECRET,{expiresIn: "1d"})
 
+        res.status(200).json({
+            message: "signin",
+            token
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
 })
 app.listen(3000, () => {
     console.log('server is running on port 3000')
