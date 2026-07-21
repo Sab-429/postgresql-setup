1cd3b9169a7b039cfabad9ad14f7cbc2bf0f665d
saysa <sabyasachi7629@gmail.com>
2026-07-21 16:46:37 +0530

    added signup endpoint

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require ('express')
-const { Pool } = require('pg')
-const pool = new Pool
+const pool = require("./db")
 const app = express() 
 app.use(express.json())
 
-app.post('/signup', (req,res) => {
+app.post('/signup', async(req,res) => {
+   try {
+        const { username, email, password}  = req.body;
+        const exitingUser = await pool.query(
+            "SELECT * FROM users WHERE email = $1", [email]
+        );
+        if(exitingUser.rows.length > 0) {
+            return res.status(409).json({
+                message: "user already exists with this email",
+            })
+        }
 
+        await pool.query(
+            "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)",
+            [
+                username,
+                email,
+                password
+            ]
+        );
+        res.status(201).json({
+            message: "SignUp successful"
+        })
+   } catch (error) {
+        res.status(500).json({
+            message: "Internal server error"
+        })
+   }
 })
 app.post('/signin', (req,res) => {
 
